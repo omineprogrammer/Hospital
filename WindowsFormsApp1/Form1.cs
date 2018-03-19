24c940f3f5096ba9073175a9ce70eb1603c979ee
omineprogrammer <thaskete_94@hotmail.com>
2018-03-19 17:27:57 -0500

    0.3.2
    Revision:
    - Estructurar Formulario de Registro:
      - Cedula, Celular: MaskedTextBox
      - Nombre, Apellido: TextBox
      - Edad: NumericUpDown
      - Fecha de Cita: MonthCalendar
    - Añadir la navegacion entre formularios:
      - Regresar al Menu Principal: Button (formularios secundarios)
      - Cerrar Sesion: Button

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -8,12 +8,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace WindowsFormsApp1
-{
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
+namespace WindowsFormsApp1 {
+    public partial class Form1 : Form {
+        public Form1() {
             InitializeComponent();
         }
     }
